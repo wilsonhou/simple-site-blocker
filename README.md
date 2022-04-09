c18914154ab114c2e46f6e408893f299e6ebfd94
Wilson Hou <wilshou@gmail.com>
2022-04-09 23:20:29 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# 🚫 A Simple Site Blocker 🚫
+
+Block sites from your browser to protect focus time (without paying for premium 🤦🏻).
+
+### Instructions
+
+1. Clone this repository with `git clone https://github.com/wilsonhou/simple-site-blocker.git`
+2. Change the `matches` list in `manifest.json` to block the sites you want.
+3. Go to `chrome://extensions`
+4. Turn on Developer Mode
+5. Click on 'Load unpacked extension'
+6. Select the folder `simple-site-blocker`
---
+++ content.js
@@ -0,0 +1,62 @@
+// get page url
+const blockPage = () => {
+  const body = document.querySelector("body");
+
+  // add a div to the body that covers the whole page
+  const cover = document.createElement("div");
+  cover.style.position = "fixed";
+  cover.style.top = "0";
+  cover.style.left = "0";
+  cover.style.width = "100%";
+  cover.style.height = "100%";
+  cover.style.backgroundColor = "rgba(0,0,0,1)";
+  cover.style.zIndex = "9999";
+  cover.style.display = "flex";
+  cover.style.justifyContent = "center";
+  cover.style.alignItems = "center";
+
+  // add a div to the cover that will contain the text
+  const text = document.createElement("div");
+  text.style.position = "absolute";
+  text.style.top = "50%";
+  text.style.left = "50%";
+  text.style.transform = "translate(-50%, -50%)";
+  text.style.color = "white";
+  text.style.fontSize = "40px";
+  text.style.fontFamily = "sans-serif";
+  text.style.textAlign = "center";
+  text.style.width = "100%";
+  text.style.height = "100%";
+  text.style.lineHeight = "1.5em";
+  text.style.zIndex = "10000";
+  text.style.display = "flex";
+  text.style.justifyContent = "center";
+  text.style.alignItems = "center";
+  text.innerText = "🚫 You've blocked this site. Probably for good reason. 🚫";
+
+  // append the text to the cover
+  cover.appendChild(text);
+
+  // append the div to the body
+  body.appendChild(cover);
+};
+
+const checkBlocked = () => {
+  for (let i = 0; i < blockedList.length; i++) {
+    if (url.includes(blockedList[i])) {
+      blockPage();
+    }
+  }
+};
+
+const url = window.location.href;
+
+const blockedList = [
+  "instagram.com",
+  "twitter.com",
+  "messenger.com",
+  "facebook.com",
+];
+
+// check if url contains substring any of the blocked sites
+checkBlocked();
